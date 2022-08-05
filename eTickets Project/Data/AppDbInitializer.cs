98eb6ef59abe13328ebd260e15aa305502347135
Nightclaw6 <sclaudiu82@yahoo.com>
2022-08-05 13:04:24 -0400

    added first db migration

---
+++ eTickets Project/Data/AppDbInitializer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace eTickets_Project.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                context.Database.EnsureCreated();
+
+                ///Locations
+                if(!context.Locations.Any())
+                {
+
+                }
+                ///Lineups
+                if (!context.Lineups.Any())
+                {
+
+                }
+                ///Festivals
+                if (!context.Festivals.Any())
+                {
+
+                }
+                ///Lineups & Festivals
+                if (!context.Lineups_Festivals.Any())
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ eTickets Project/Migrations/20220805154625_Initial.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eTickets_Project.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lineups",
+                columns: table => new
+                {
+                    BandId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BandLogo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BandName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BandBio = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lineups", x => x.BandId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    LocationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.LocationId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Festivals",
+                columns: table => new
+                {
+                    FestivalId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FestivalName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FestivalDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    FestivalLogoURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Genre = table.Column<int>(type: "int", nullable: false),
+                    LocationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Festivals", x => x.FestivalId);
+                    table.ForeignKey(
+                        name: "FK_Festivals_Locations_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Locations",
+                        principalColumn: "LocationId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lineups_Festivals",
+                columns: table => new
+                {
+                    FestivalId = table.Column<int>(type: "int", nullable: false),
+                    LineupId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lineups_Festivals", x => new { x.LineupId, x.FestivalId });
+                    table.ForeignKey(
+                        name: "FK_Lineups_Festivals_Festivals_FestivalId",
+                        column: x => x.FestivalId,
+                        principalTable: "Festivals",
+                        principalColumn: "FestivalId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Lineups_Festivals_Lineups_LineupId",
+                        column: x => x.LineupId,
+                        principalTable: "Lineups",
+                        principalColumn: "BandId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Festivals_LocationId",
+                table: "Festivals",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lineups_Festivals_FestivalId",
+                table: "Lineups_Festivals",
+                column: "FestivalId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lineups_Festivals");
+
+            migrationBuilder.DropTable(
+                name: "Festivals");
+
+            migrationBuilder.DropTable(
+                name: "Lineups");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+        }
+    }
+}
